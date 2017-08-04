9a7cebbc30a66321d3c83ab3d1c11256499d73b3
Ananya-96 <ananyaramesh96@gmail.com>
2017-08-04 22:12:24 +0530

    first

---
+++ splash/ventures/js/main.js
@@ -0,0 +1,17 @@
+window.onload=function() {
+	$(".parallax1").click(function () {
+
+	    $header = $(this);
+	    //getting the next element
+	    $content = $header.next();
+	    //open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
+	    $content.slideToggle(500, function () {
+	        //execute this after slideToggle is done
+	        //change text of header based on visibility of content div
+
+	    });
+
+	});
+
+
+}
