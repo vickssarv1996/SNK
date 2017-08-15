d23e2e65fe5f751c0c15feb4138646b71ba8ab2a
Ananya-96 <ananyaramesh96@gmail.com>
2017-08-15 21:04:23 +0530

    complete

--- splash/js/homepage.js
+++ splash/js/homepage.js
@@ -12,3 +12,11 @@ function showSlides() {
     slides[slideIndex-1].style.display = "block";
     setTimeout(showSlides, 7000); // Change image every 2 seconds
 }
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
